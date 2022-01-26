644d0ae99e56466117d09b14f89e6eb9f0abc4d0
Mariano Uriel Delgado <marianoudelgado@gmail.com>
2022-01-26 16:32:01 -0300

    First Step, Create a proyect

---
+++ SomosMasApp/SomosMasApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomosMasApp
+//
+//  Created by Mariano Uriel Delgado on 26/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
