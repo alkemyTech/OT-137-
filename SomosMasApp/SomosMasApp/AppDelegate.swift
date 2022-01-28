7b31a9cef09dcffb5474f7d663a18aeba5716689
Mariano Uriel Delgado <marianoudelgado@gmail.com>
2022-01-28 13:31:15 -0300

    Correcion que me paso David

--- SomosMasApp/SomosMasApp/AppDelegate.swift
+++ SomosMasApp/SomosMasApp/AppDelegate.swift
@@ -14,14 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        Thread.sleep(forTimeInterval: 5.0)
-        
-        print(Bundle.main.infoDictionary?["BASE_URL"])
         
         return true
     }
-    
-    
 
     // MARK: UISceneSession Lifecycle
 
--- SomosMasApp/SomosMasApp/ViewController.swift
+++ SomosMasApp/SomosMasApp/ViewController.swift
@@ -13,23 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        fetchFilms()
 
-      
-        
-        //getData()
-        // Do any additional setup after loading the view.
-    }
-    
-    func fetchFilms() {
-        // 1
-        let request = AF.request()
-        // 2
-        request.responseJSON { (data) in
-          print(data)
         }
-      }
+       
+    }
+
    
 
-}
+
 
