fbd5ba17ff9447869f29b156c795027744b53f50
Mariano Uriel Delgado <marianoudelgado@gmail.com>
2022-01-28 09:27:52 -0300

    Eliminando carpeta Pods

--- SomosMasApp/Pods/Target Support Files/Pods-SomosMasApp-SomosMasAppUITests/Pods-SomosMasApp-SomosMasAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SomosMasApp_SomosMasAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SomosMasApp_SomosMasAppUITests
-@end
--- SomosMasApp/Pods/Target Support Files/Pods-SomosMasApp/Pods-SomosMasApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SomosMasApp : NSObject
-@end
-@implementation PodsDummy_Pods_SomosMasApp
-@end
