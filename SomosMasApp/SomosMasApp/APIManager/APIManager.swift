0f2cd3fbcc506f4bf2734ef6231e4d25f413b7ae
Mariano Uriel Delgado <marianoudelgado@gmail.com>
2022-01-28 12:52:19 -0300

    Agrego el Podfile

---
+++ SomosMasApp/SomosMasApp/APIManager/APIManager.swift
@@ -0,0 +1,8 @@
+//
+//  APIManager.swift
+//  SomosMasApp
+//
+//  Created by Mariano Uriel Delgado on 28/01/2022.
+//
+
+import Foundation
---
+++ SomosMasApp/SomosMasApp/Model/RegisterModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterModel.swift
+//  SomosMasApp
+//
+//  Created by Mariano Uriel Delgado on 28/01/2022.
+//
+
+import Foundation
