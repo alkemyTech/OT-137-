7f5f354149cf15b1a28901c34d45d6116d356244
Rodrigo Tores <rtorres@munidigital.com>
2022-01-30 23:44:26 -0300

    Setup LoginViewModel

--- SomosMasApp/SomosMasApp/Use Cases/Login/ViewModel/LoginViewModel.swift
+++ SomosMasApp/SomosMasApp/Use Cases/Login/ViewModel/LoginViewModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginViewModel {
+    
+    private var credentials = Credentials() {
+        didSet {
+            email = credentials.email
+            password = credentials.password
+        }
+    }
+    
+    private var email = ""
+    private var password = ""
+    private var isValidEmail = false
+    private var isValidPassword = false
+    
+    var isButtonLoginShow: Observable<Bool> = Observable(false)
+    
+    func validateTextFields(email: String, password: String) {
+        isValidEmail = email.isValidEmail ? true : false
+        isValidPassword = password.isValidPassowrd ? true : false
+    }
+    
+    func textFieldsInput() -> LoginButtonStatus {
+        if isValidEmail && isValidPassword {
+            isButtonLoginShow.value = true
+            return .Show
+        }
+        return .Hide
+    }
+    
+}
+
+extension LoginViewModel {
+    enum LoginButtonStatus {
+        case Show
+        case Hide
+    }
+}
