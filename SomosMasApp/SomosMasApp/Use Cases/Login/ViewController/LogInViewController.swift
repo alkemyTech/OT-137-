959737be0f70f04c3590bc0c88e48af5f40dbbe7
Sancara <santiagocaraballo@gmail.com>
2022-02-02 10:26:01 -0300

    landscape solved and navigation to signUp

---
+++ SomosMasApp/SomosMasApp/Use Cases/Login/ViewController/LogInViewController.swift
@@ -0,0 +1,108 @@
+//
+//  LogInViewController.swift
+//  SomosMasApp
+//
+//  Created by Santiago Caraballo on 27/01/2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    var loginViewModel = LoginViewModel()
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var credentials: UIView!
+    
+    
+    let somosMasImageView: UIImageView = {
+        let logo = UIImage(named: "LOGO-SOMOS-MAS")
+        let imageView = UIImageView(image: logo)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hideLoginButton()
+        setupTextFields()
+        bindData()
+        
+        //UI LogInLayout
+        
+        setupLayout()
+        
+        
+    }
+    
+    //MARK: Button Action
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func createButtonPressed(_ sender: UIButton) {
+        let signUpVC = SignUpViewController(nibName: "SignUpViewController", bundle: Bundle.main)
+        self.navigationController?.pushViewController(signUpVC, animated: true)
+    }
+    
+    func bindData() {
+        loginViewModel.isButtonLoginShow.bind { [weak self] in
+            $0 ? self?.showLoginButton() : self?.hideLoginButton()
+        }
+        
+    }
+    
+    func showLoginButton() {
+        loginButton.isEnabled = true
+        loginButton.backgroundColor = .red
+    }
+    
+    // MARK: Buttons Setup
+    private func hideLoginButton() {
+        loginButton.isEnabled = false
+        loginButton.backgroundColor = .clear
+    }
+    
+    
+    // MARK: Text Fields Setup
+    func setupTextFields() {
+        emailTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        if let emailText = emailTextField.text, let passwordText = passwordTextField.text {
+            loginViewModel.validateEmail(email: emailText, password: passwordText)
+        }
+        
+        loginViewModel.textFieldsInput()
+
+    }
+    
+    // Layout Setup
+    private func setupLayout() {
+        let topImageContainerView = UIView()
+        view.addSubview(topImageContainerView)
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+       
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25).isActive = true
+        topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        topImageContainerView.addSubview(somosMasImageView)
+        
+        somosMasImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        somosMasImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+
+        credentials.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor, constant: 40).isActive = true
+        
+    }
+    
+
+}
--- SomosMasApp/SomosMasApp/Use Cases/SignUp/SignUpViewController.swift
+++ SomosMasApp/SomosMasApp/Use Cases/SignUp/SignUpViewController.swift
@@ -1,13 +1,13 @@
 //
-//  LogInViewController.swift
+//  SignUpViewController.swift
 //  SomosMasApp
 //
-//  Created by Santiago Caraballo on 27/01/2022.
+//  Created by Santiago Caraballo on 02/02/2022.
 //
 
 import UIKit
 
-class LogInViewController: UIViewController {
+class SignUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
