84e067db10166600068463aaaffb17194e2ea9ac
Rodrigo Tores <rtorres@munidigital.com>
2022-02-06 19:42:16 -0300

    Add constants url and endpoints

---
+++ SomosMasApp/SomosMasApp/Helpers/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  SomosMasApp
+//
+//  Created by Rodrigo Torres on 06/02/2022.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct URL {
+        
+        static let BASE_URL = "http://ongapi.alkemy.org/api"
+        
+        
+        struct Endpoints {
+            
+            static let LOGIN = "/login"
+            
+        }
+    }
+    
+}
