1da0889acb41612f3248b263b573bd7fb6549511
Mariano Uriel Delgado <marianoudelgado@gmail.com>
2022-02-07 13:46:17 -0300

    Enviroment VAR

--- SomosMasApp/SomosMasApp/AppDelegate.swift
+++ SomosMasApp/SomosMasApp/AppDelegate.swift
@@ -11,13 +11,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    //Variable de entorno
+    
+    let _ = "Aca va la variable de Entorno"
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         return true
     }
+    
+    
 
     // MARK: UISceneSession Lifecycle
 
