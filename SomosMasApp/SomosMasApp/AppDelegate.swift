08adbf773fb0725bcd464a240159fda07e593710
Mariano Uriel Delgado <marianoudelgado@gmail.com>
2022-02-07 15:45:20 -0300

    Como Usar la Variable de entorno

--- SomosMasApp/SomosMasApp/AppDelegate.swift
+++ SomosMasApp/SomosMasApp/AppDelegate.swift
@@ -11,9 +11,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    //Variable de entorno
-    
-    let _ = "Aca va la variable de Entorno"
+
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
