4f461b1e3243c762b350f3039857fedef77a4ad5
Rodrigo Tores <rtorres@munidigital.com>
2022-02-23 23:07:15 -0300

    Add environment variable

--- SomosMasApp/SomosMasApp/Service/APIManager.swift
+++ SomosMasApp/SomosMasApp/Service/APIManager.swift
@@ -14,10 +14,11 @@ class APIManager {
     
     func loginUser(email: String, password: String,  sucess: @escaping (_ loginDataResponse: LoginUserResponse) -> (), failure: @escaping ( _ error: Error?) -> () ) {
         
-        let url = Constants.URL.BASE_URL+Constants.URL.Endpoints.LOGIN
         let params: [String: String] = ["email": email, "password": password]
+        let serverUrl = Bundle.main.object(forInfoDictionaryKey: "ServerURL") as! String
+        let finalUrl = serverUrl + "login"
         
-        AF.request(url, method: .post, parameters: params,  encoder: JSONParameterEncoder.default).validate(statusCode: 200...299).responseDecodable(of: LoginUserResponse.self) {
+        AF.request(finalUrl, method: .post, parameters: params,  encoder: JSONParameterEncoder.default).validate(statusCode: 200...299).responseDecodable(of: LoginUserResponse.self) {
             response in
             if let userResponse = response.value {
                 sucess(userResponse)
