77697104c87b1e3bfc103a730f72f9413ada8348
Mariano Uriel Delgado <marianoudelgado@gmail.com>
2022-03-02 13:01:03 -0300

    Card finalizada, con slider

---
+++ SomosMasApp/SomosMasApp/Use Cases/Home/CollectionCell/SliderCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SliderCollectionViewCell.swift
+//  SomosMasApp
+//
+//  Created by Mariano Uriel Delgado on 02/03/2022.
+//
+
+import UIKit
+
+class SliderCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleCollectionViewCell: UILabel!
+    @IBOutlet weak var imageCollectionViewCell: UIImageView!
+    @IBOutlet weak var descriptionCollectionViewCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        descriptionCollectionViewCell.lineBreakMode = .byWordWrapping
+        descriptionCollectionViewCell.numberOfLines = 0
+    }
+
+}
